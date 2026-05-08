8a5a4ff77b38e28ed277980c15ae26648c923e0e
alekbark <olegbarkunovg@gmail.com>
2026-05-08 15:57:20 +0500

    add hw15 to l3/config/first_app

--- l3/config/first_app/views.py
+++ l3/config/first_app/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+def api(request):
+    data = [i for i in range(10)]
+    return JsonResponse(data, safe=False)
